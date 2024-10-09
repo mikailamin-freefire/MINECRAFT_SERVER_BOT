753820c1836cdf0f808d27619b61f4788e30c726
mikailamin-freefire <mikailamin.freefire@gmail.com>
2024-10-09 18:40:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,120 +6,50 @@ const { pathfinder, Movements, goals} = require('mineflayer-pathfinder')
 const armorManager = require('mineflayer-armor-manager')
 const mc = require('minecraft-protocol');
 const AutoAuth = require('mineflayer-auto-auth');
+
 const app = express();
-
-
 app.use(express.json());
 
-app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (_, res) => res.send("Bot is now starting..."));
 app.listen(process.env.PORT);
 
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.repl.co/`);
-}, 224000);
+const server_ip = 'mikailserver.aternos.me';
+const server_port = 30505;
+const bot_username = 'Minecraft_AI';
 
+function initialize_bot() {
+    app.get("/", (_, res) => res.send("Bot Joining..."));
+    app.listen(process.env.PORT);
+    
+    const bot = mineflayer.createBot({
+        host: server_ip, 
+        version: false,
+        username: bot_username,
+        port: server_port,
+        plugins: [AutoAuth],
+        AutoAuth: 'bot112022'
+    });
 
-// U CAN ONLY EDIT THIS SECTION!!
-function createBot () {
-const bot = mineflayer.createBot({
-  host: 'mikailserver.aternos.me', 
-  version: false, // }U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'ExplosiveKiller',
-  port: 30505, 
-  plugins: [AutoAuth],
-  AutoAuth: 'bot112022'
-})
-/// DONT TOUCH ANYTHING MORE!
-bot.loadPlugin(pvp)
-bot.loadPlugin(armorManager)
-bot.loadPlugin(pathfinder)
+    bot.loadPlugin(pvp);
+    bot.loadPlugin(pathfinder);
 
+    bot.on('physicTick', () => {
+        const filter = entity => entity.type === 'mob' && entity.displayName === 'Zombie' || entity.displayName === 'Creeper' || entity.displayName === 'Skeleton' || entity.displayName === 'Spider';
+        const entity = bot.nearestEntity(filter);
+        if (entity) bot.pvp.attack(entity);
+    });
 
-bot.on('playerCollect', (collector, itemDrop) => {
-  if (collector !== bot.entity) return
+    bot.on('login', (spawn) => {
+        console.log("Congratulations, your bot has been logged in to the server!");
+        app.get("/", (_, res) => res.send("Your bot has now live"));
+        app.listen(process.env.PORT);
+    });
 
-  setTimeout(() => {
-    const sword = bot.inventory.items().find(item => item.name.includes('sword'))
-    if (sword) bot.equip(sword, 'hand')
-  }, 150)
-})
-
-bot.on('playerCollect', (collector, itemDrop) => {
-  if (collector !== bot.entity) return
-
-  setTimeout(() => {
-    const shield = bot.inventory.items().find(item => item.name.includes('shield'))
-    if (shield) bot.equip(shield, 'off-hand')
-  }, 250)
-})
-
-let guardPos = null
-
-function guardArea (pos) {
-  guardPos = pos.clone()
-
-  if (!bot.pvp.target) {
-    moveToGuardPos()
-  }
+    bot.on('end', (reason) => {
+        console.error("Bot has been disconnected from the server!");
+        console.log("Restarting...");
+        initialize_bot();
+    });
 }
 
-function stopGuarding () {
-  guardPos = null
-  bot.pvp.stop()
-  bot.pathfinder.setGoal(null)
-}
-
-function moveToGuardPos () {
-  const mcData = require('minecraft-data')(bot.version)
-  bot.pathfinder.setMovements(new Movements(bot, mcData))
-  bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
-}
-
-bot.on('stoppedAttacking', () => {
-  if (guardPos) {
-    moveToGuardPos()
-  }
-})
-
-bot.on('physicTick', () => {
-  if (bot.pvp.target) return
-  if (bot.pathfinder.isMoving()) return
-
-  const entity = bot.nearestEntity()
-  if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0))
-})
-bot.on('physicTick', () => {
-  if (!guardPos) return
-  const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
-                      e.mobType !== 'Armor Stand' 
-  const entity = bot.nearestEntity(filter)
-  if (entity) {
-    bot.pvp.attack(entity)
-  }
-})
-bot.on('chat', (username, message) => {
-  if (message === 'guard') {
-    const player = bot.players[username]
-
-    if (!player) {
-    bot.chat('I will!')
-    guardArea(player.entity.position)
-    }
-
-  }
-  if (message === 'stop') {
-    bot.chat('I will stop!')
-    stopGuarding()
-  }
-})
-
-bot.on('kicked', console.log)
-bot.on('error', console.log)
-bot.on('end', createBot)
-}
-
-createBot()
-
-//// Rembember to sucribe to my channels!
-/// www.youtube.com/c/JinMoriYT
-///www.youtube.com/channel/UC1SR0lQSDfdaSMhmUiaMitg
+initialize_bot();
