7677b71ae6b6927a9e2cb115496de546ab8a7e6d
mikailamin-freefire <mikailamin.freefire@gmail.com>
2024-10-09 15:58:02 +0600

    first uploaded files

---
+++ index.js
@@ -0,0 +1,39 @@
+const mineflayer = require('mineflayer');
+const AutoAuth = require('mineflayer-auto-auth');
+const pvp = require('mineflayer-pvp').plugin;
+
+const server_ip = 'mikailserver.aternos.me';
+const server_port = 30505;
+const bot_username = 'Minecraft_AI';
+
+function initialize_bot() {
+    const bot = mineflayer.createBot({
+        host: server_ip, 
+        version: false,
+        username: bot_username,
+        port: server_port,
+        plugins: [AutoAuth],
+        AutoAuth: 'bot112022'
+    });
+
+    bot.loadPlugin(pvp);
+    bot.loadPlugin(pathfinder);
+
+    bot.on('physicTick', () => {
+        const filter = entity => entity.type === 'mob' && entity.displayName === 'Zombie' || entity.displayName === 'Creeper' || entity.displayName === 'Skeleton' || entity.displayName === 'Spider';
+        const entity = bot.nearestEntity(filter);
+        if (entity) bot.pvp.attack(entity);
+    });
+
+    bot.on('login', (spawn) => {
+        console.log("Congratulations, your bot has been logged in to the server!");
+    });
+
+    bot.on('end', (reason) => {
+        console.error("Bot has been disconnected from the server!");
+        console.log("Restarting...");
+        initialize_bot();
+    });
+}
+
+initialize_bot();
