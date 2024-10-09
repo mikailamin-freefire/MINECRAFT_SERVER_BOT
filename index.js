532fbd4e7dfaae2d2f05f939ec90cde1bc5d1a34
mikailamin-freefire <mikailamin.freefire@gmail.com>
2024-10-09 16:06:29 +0600

    added variables

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const mineflayer = require('mineflayer');
 const AutoAuth = require('mineflayer-auto-auth');
 const pvp = require('mineflayer-pvp').plugin;
 
-const server_ip = 'mikailserver.aternos.me';
-const server_port = 30505;
-const bot_username = 'Minecraft_AI';
+const server_ip = process.env.SERVER_IP;
+const server_port = process.env.SERVER_PORT;
+const bot_username = process.env.BOT_USERNAME;
 
 function initialize_bot() {
     const bot = mineflayer.createBot({
-        host: server_ip, 
+        host: server_ip,
         version: false,
         username: bot_username,
         port: server_port,
@@ -20,16 +20,16 @@ function initialize_bot() {
     bot.loadPlugin(pathfinder);
 
     bot.on('physicTick', () => {
-        const filter = entity => entity.type === 'mob' && entity.displayName === 'Zombie' || entity.displayName === 'Creeper' || entity.displayName === 'Skeleton' || entity.displayName === 'Spider';
+        const filter = entity => entity.type === 'mob' && (entity.displayName === 'Zombie' || entity.displayName === 'Creeper' || entity.displayName === 'Skeleton' || entity.displayName === 'Spider');
         const entity = bot.nearestEntity(filter);
         if (entity) bot.pvp.attack(entity);
     });
 
-    bot.on('login', (spawn) => {
+    bot.on('login', () => {
         console.log("Congratulations, your bot has been logged in to the server!");
     });
 
-    bot.on('end', (reason) => {
+    bot.on('end', () => {
         console.error("Bot has been disconnected from the server!");
         console.log("Restarting...");
         initialize_bot();
